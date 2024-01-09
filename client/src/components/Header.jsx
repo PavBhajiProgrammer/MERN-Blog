0f915cf825dc88857c394bee1cd09b5d650233a8
pavbhajiprogrammer <pavbhajiprogrammer@gmail.com>
2024-01-09 19:23:26 +0530

    project starter files added

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div>
+       <nav>
+        <div className="container nav__container">
+            <Link to="/" className="nav__logo">
+                <img src="" alt="" />
+            </Link>
+        </div>
+       </nav>
+    </div>
+  )
+}
+
+export default Header
